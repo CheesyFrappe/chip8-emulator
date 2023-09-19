7f311098548b44eb14a2726820f2319dd3b68326
Emir Balcı <aerangalerind@hotmail.com>
2023-09-19 12:24:53 +0300

    Implement display

--- src/main.c
+++ src/main.c
@@ -2,75 +2,92 @@
 #include <stdbool.h>
 #include "SDL2/SDL.h"
 #include "chip8.h"
+#include "chip8keyboard.h"
 
 const char keyboard_map[CHIP8_TOTAL_KEYS] = {
     SDLK_0, SDLK_1, SDLK_2, SDLK_3, SDLK_4, SDLK_5,
     SDLK_6, SDLK_7, SDLK_8, SDLK_9, SDLK_a, SDLK_b,
-    SDLK_c, SDLK_d, SDLK_e, SDLK_f  
-};
+    SDLK_c, SDLK_d, SDLK_e, SDLK_f};
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     struct chip8 chip8;
     chip8_init(&chip8); // initialize chip8 by setting its value NULL
 
+    chip8_screen_set(&chip8.screen, 0, 0);
+
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *window = SDL_CreateWindow(
         EMULATOR_WINDOW_TITLE,
-        SDL_WINDOWPOS_UNDEFINED,    // undefined position for x-axis
-        SDL_WINDOWPOS_UNDEFINED,    // undefined position for x-axis
-        CHIP8_WINDOW_MULTIPLIER * CHIP8_WIDTH, 
-        CHIP8_WINDOW_MULTIPLIER * CHIP8_HEIGHT, 
-        SDL_WINDOW_SHOWN
-    );
+        SDL_WINDOWPOS_UNDEFINED, // undefined position for x-axis
+        SDL_WINDOWPOS_UNDEFINED, // undefined position for x-axis
+        CHIP8_WINDOW_MULTIPLIER * CHIP8_WIDTH,
+        CHIP8_WINDOW_MULTIPLIER * CHIP8_HEIGHT,
+        SDL_WINDOW_SHOWN);
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
 
-    while(1)
-    {   
+    while (1)
+    {
         SDL_Event event;
-        while(SDL_PollEvent(&event))    // handles events by polling events from library and storing it in variable
+        while (SDL_PollEvent(&event)) // handles events by polling events from library and storing it in variable
         {
             switch (event.type)
             {
-                case SDL_QUIT:
-                    goto out;
+            case SDL_QUIT:
+                goto out;
                 break;
 
-                case SDL_KEYDOWN:
+            case SDL_KEYDOWN:
+            {
+                char key = event.key.keysym.sym;
+                int vkey = chip8_keyboard_map(keyboard_map, key);
+                if (vkey != -1)
                 {
-                    char key = event.key.keysym.sym;
-                    int vkey = chip8_keyboard_map(keyboard_map, key);
-                    if (vkey != -1)
-                    {
-                        chip8_keyboard_down(&chip8.keyboard, vkey);
-                    }
+                    chip8_keyboard_down(&chip8.keyboard, vkey);
                 }
-                break;
+            }
+            break;
 
-                case SDL_KEYUP:
+            case SDL_KEYUP:
+            {
+                char key = event.key.keysym.sym;
+                int vkey = chip8_keyboard_map(keyboard_map, key);
+                if (vkey != -1)
                 {
-                    char key = event.key.keysym.sym;
-                    int vkey = chip8_keyboard_map(keyboard_map, key);
-                    if (vkey != -1)
-                    {
-                        chip8_keyboard_up(&chip8.keyboard, vkey);
-                    }
+                    chip8_keyboard_up(&chip8.keyboard, vkey);
                 }
-                break;
+            }
+            break;
+            };
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0); // alpha value followed by RGB values
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+
+        for (int x = 0; x < CHIP8_WIDTH; x++)
+        {
+            for (int y = 0; y < CHIP8_HEIGHT; y++)
+            {
+                if (chip8_screen_is_set(&chip8.screen, x, y))
+                {
+                    /*
+                        CHIP8 emulator has a 64x32 display.
+                        Since this is very small for today's machines, it should be displayed 10x bigger.
+                        Hence, coordinates and sizes of the pixel is multiplied by 'CHIP8_WINDOW_MULTIPLIER'.
+                    */
+                    SDL_Rect r;
+                    r.x = x * CHIP8_WINDOW_MULTIPLIER;  
+                    r.y = y * CHIP8_WINDOW_MULTIPLIER;
+                    r.w = CHIP8_WINDOW_MULTIPLIER;
+                    r.h = CHIP8_WINDOW_MULTIPLIER;
+                    SDL_RenderFillRect(renderer, &r);
+                }
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);  // alpha value followed by RGB values
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
-        SDL_Rect r;
-        r.x = 0;
-        r.y = 0;
-        r.w = 40;
-        r.h = 40;
-        SDL_RenderFillRect(renderer, &r);
-        SDL_RenderPresent(renderer);    // apply the changes
+        SDL_RenderPresent(renderer); // apply the changes
     }
 
 out:
